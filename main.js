78c7f4a223ad7398368ce957239474ea6ef44cbb
Lydia Lind <lydia.nilsson@gmail.com>
2020-01-04 12:44:52 +0100

    Total refactoring
    Have acomplished a game board and flipping the cards

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
-var brickImages = ['blossom.jpg', 'cat.jpg', 'catNdog.jpg', 'horses.jpg', 'kitty.jpg', 'sea.jpg', 'blossom.jpg', 'cat.jpg', 'catNdog.jpg', 'horses.jpg', 'kitty.jpg', 'sea.jpg'];
-brickImages.sort(() => Math.random() - 0.5);
+let bricks = document.querySelectorAll('.brick');
+let hasFlipped = false;
+let firstCard, secondCard;
 
-var bricksOpened = [];
-var count = 0;
-
-var allBricks = document.getElementsByClassName('brick');
-
-for(let brick of allBricks){
-    brick.addEventListener('click', checkImage);
-}
-
-function checkImage(event){
-    var currentBrick = event.target;
-    var newSrc = 'brickImages' + '\\' + brickImages[Number(currentBrick.getAttribute('id'))]
-    bricksOpened.push(currentBrick);
-    currentBrick.setAttribute('src', newSrc);
-    currentBrick.removeEventListener('click', checkImage);
-    if(bricksOpened.length == 2){
-        checkIfBricksAreTheSame();
+function flipCard(){
+    if(!hasFlipped){
+        firstCard = this;
+        hasFlipped = true;
+        firstCard.removeEventListener('click', flipCard);
     }
+    else{
+        secondCard = this;
+    }
+    console.log(firstCard);
+    console.log(secondCard);
 }
 
-function checkIfBricksAreTheSame(){
-    setTimeout(function(){
-        if(bricksOpened[0].getAttribute('src') == bricksOpened[1].getAttribute('src')){
-            bricksOpened[0].style.visibility = 'hidden';
-            bricksOpened[1].style.visibility = 'hidden';
-            bricksOpened = [];
-        } 
-        else{
-            bricksOpened[0].setAttribute('src', 'brickImages\\default.jfif');
-            bricksOpened[1].setAttribute('src', 'brickImages\\default.jfif');
-            bricksOpened[0].addEventListener('click', checkImage);
-            bricksOpened[1].addEventListener('click', checkImage);
-            bricksOpened = [];
-        }
-    }, 1000)
-}
+bricks.forEach(brick => brick.addEventListener('click', flipCard));
