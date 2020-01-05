9697901fee7bccfbc8e82d6f902be223268924f5
irissiri7 <48020183+irissiri7@users.noreply.github.com>
2020-01-05 12:18:42 +0100

    Small fixes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let bricks = document.querySelectorAll('.brick');
 let hasFlipped = false;
 let firstCard, secondCard;
 let pairsFound = 0;
-lockGame = false;
+let lockGame = false;
 
 function flipCard(){
     if(lockGame) return;
@@ -17,27 +17,29 @@ function flipCard(){
         secondCard.classList.add('flip');
         secondCard.removeEventListener('click', flipCard);
         lockGame = true;
-        if(firstCard.dataset.name === secondCard.dataset.name){
-            pairsFound++;
-            console.log(pairsFound);
+        checkMatch();
+    }
+}
+function checkMatch(){
+    if(firstCard.dataset.name === secondCard.dataset.name){
+        pairsFound++;
+        hasFlipped = false;
+        lockGame = false;
+        if(pairsFound == 6){
+            setTimeout(()=>{
+                document.getElementById('win-container').style.visibility = 'visible';
+            },1000)
+        }
+    }
+    else{
+        setTimeout(()=>{
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            firstCard.addEventListener('click', flipCard);
+            secondCard.addEventListener('click', flipCard);
             hasFlipped = false;
             lockGame = false;
-            if(pairsFound == 6){
-                setTimeout(()=>{
-                    document.getElementById('win-container').style.visibility = 'visible';
-                },1000)
-            }
-        }
-        else{
-            setTimeout(()=>{
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-                firstCard.addEventListener('click', flipCard);
-                secondCard.addEventListener('click', flipCard);
-                hasFlipped = false;
-                lockGame = false;
-            }, 1500)
-        }
+        }, 1500)
     }
 }
 
